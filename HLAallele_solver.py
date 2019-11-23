a3b96695b354c24c5a6dc61d17fa9bbb2c89add6
bigguscodus <mikka_tolmachev@protonmail.com>
2019-11-23 12:49:00 +0300

    Добавленны комменты, исправлена запись уникальных аллелей

--- HLAallele_solver.py
+++ HLAallele_solver.py
@@ -1,14 +1,12 @@
 import pandas as pd
-
-
 def hla_allele_solver(data_frame: pd.DataFrame) -> tuple:
     '''This function takes dataframe as an argument and returns tuple with two lists. Each list contains set of allele according to their ambiguity'''
-    ambiguity_alleles = []
-    uniq_alleles = []
-    solved_alleles = []
+    ambiguity_alleles = [] # лист для хранения множеств неразрешимых аллелей
+    uniq_alleles = [] # лист для хранения уникальных аллелей
+    solved_alleles = [] # лист для хранения решенных аллелей, чтобы лишний раз их не сравнивать со всеми
     for allele_name in data_frame.columns:
         if allele_name in solved_alleles:
-            continue
+            continue # если аллель мы решили, то дальше с ней работь
         data_temp = data_frame.copy()
         for i in data_temp.index:
             if pd.isna(data_temp.loc[i, allele_name]):
