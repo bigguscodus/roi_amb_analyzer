4e266d5d9acc6fa8cb62fdfb1438b3c8af24dcea
bigguscodus <mikka_tolmachev@protonmail.com>
2019-12-11 13:27:49 +0300

    Добавлен ключ для разрешения

--- HLAallele_solver.py
+++ HLAallele_solver.py
@@ -1,10 +1,18 @@
 import pandas as pd
-def hla_allele_solver(data_frame: pd.DataFrame) -> tuple:
+import re
+def hla_allele_solver(data_frame: pd.DataFrame, resolution:str) -> tuple:
     '''This function takes dataframe as an argument and returns tuple with two lists. Each list contains set of allele according to their ambiguity'''
+    first_level = r'HLA-[A-Z]*\*[0-1]*'
+    second_level = r'HLA-[A-Z]*\*[0-9]*:[0-9]*'
+    third_level = r'HLA-[A-Z]*\*[0-9]*:[0-9]*:[0-9]*'
+    fourth_level = r'HLA-[A-Z]*\*[0-9]*:[0-9]*:[0-9]*:[0-9]*'
+    resolutiion_image = {'1':first_level,'2':second_level,'3':third_level,'4':fourth_level}
     ambiguity_alleles = [] # лист для хранения множеств неразрешимых аллелей
     uniq_alleles = [] # лист для хранения уникальных аллелей
     solved_alleles = [] # лист для хранения решенных аллелей, чтобы лишний раз их не сравнивать со всеми
     for allele_name in data_frame.columns:
+        prefix_set = set()
+        after_set = set() # хранит множество неразрешимых аллелей после детализации по разрешению
         if allele_name in solved_alleles:
             continue # если аллель мы решили, то дальше с ней работь
         data_temp = data_frame.copy()
@@ -19,7 +27,14 @@ def hla_allele_solver(data_frame: pd.DataFrame) -> tuple:
         set_of_names = set(data_temp.columns)
         solved_alleles.extend(set_of_names)
         if len(set_of_names) > 1:
-            ambiguity_alleles.append(set_of_names)  # если множество больше 1, добавляем в неразрешимые неоднозначности
+            for allele in set_of_names:
+                prefix = re.findall(resolutiion_image[resolution], allele)
+                if prefix == []: # если у аллели нет нужного разрешения, оставляем как есть
+                    prefix = [allele]
+                if prefix[0] not in prefix_set:
+                    prefix_set.add(prefix[0])
+                    after_set.add(allele)
+            ambiguity_alleles.append(after_set)  # если множество больше 1, добавляем в неразрешимые неоднозначности
         else:
             uniq_alleles.append(set_of_names)
     return (ambiguity_alleles,uniq_alleles)
--- roi_amb_analyzer.py
+++ roi_amb_analyzer.py
@@ -6,15 +6,16 @@ from HLA_allele_writer import hla_allele_writer
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--imgt_file', default='hla.dat', help='Enter path to the database')
-parser.add_argument('-r', '--region', help="HLA prefix and gene. Example: HLA-A", required=True)
+parser.add_argument('-g', '--gene', help="HLA prefix and gene. Example: HLA-A", required=True)
 parser.add_argument('-e', '--exon_numbers', help='Enter the numbers of exons, separated by space. Example: 2 3 4',
                     nargs='+', required=True)
 parser.add_argument('-a', '--amb',
                     help='Enter the path for ambiguous alleles output. Example ~/Documents/amb')
 parser.add_argument('-u', '--unq',
                     help='Enter the path for nonambiguous alleles output. Example ~/Documents/uni')
+parser.add_argument('-r', '--resolution', default='4', required=True, help='Determines the level of detail of the alleles in a result according to nomenclature. Levels are: 1,2,3,4')
 args = parser.parse_args()
-allele_exons_seqs = hla_parser(data=args.imgt_file, region=args.region)
+allele_exons_seqs = hla_parser(data=args.imgt_file, region=args.gene)
 data_frame = hla_filter_framer(allele_exon_dict=allele_exons_seqs, exon_numbers=args.exon_numbers)
-allele_lists = hla_allele_solver(data_frame=data_frame)
+allele_lists = hla_allele_solver(data_frame=data_frame,resolution=args.resolution)
 hla_allele_writer(allele_lists=allele_lists, amb_allele_file=args.amb, uniq_allele_file=args.unq)
