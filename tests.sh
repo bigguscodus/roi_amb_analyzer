b36d10f3094b9a9551925642a0489d033ba21d53
bigguscodus <mikka_tolmachev@protonmail.com>
2020-01-02 17:39:13 +0300

    Пытаюсь настроить jenkins

--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-. ~/PycharmProjects/parseq_lab/venv/bin/activate
+. /home/mikka/PycharmProjects/parseq_lab/venv/bin/activate
 PYTHONPATH=. python test_hla_secondary_functions.py 
 
 
